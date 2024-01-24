95e39888921f4a1647f14bc44a9cd5107b48af93
Dimitar Mitev <72569055+mitko8009@users.noreply.github.com>
2024-01-24 21:54:22 +0200

    Rewritten rendering fun | Bug fix

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
-MOD_NAME = "Sprint Mod"
-VERSION = "1.11"
-AUTHOR = "mitko8009"
-SOCIAL = "@mitko8009_"
+Sprint = RegisterMod("Sprint Mod", 1);
+local mod = Sprint;
+Sprint.MOD_NAME = "Sprint Mod"
+Sprint.VERSION = "1.13"
+Sprint.AUTHOR = "mitko8009"
+Sprint.SOCIAL = "@mitko8009_"
 
-local SprintMod = RegisterMod(MOD_NAME, 1);
-local mod = SprintMod;
 
 local json = require("json");
 
@@ -21,7 +21,7 @@ mc_ypos = {195, 206, 217}
 --- Config ---
 --------------
 
-settings = {
+settings_SM = {
     -- Base settings
     MAX_STAMINA = mc_max_stamina[2],
     STAMINA_ADDER = mc_stamina_adder[5],
@@ -31,15 +31,11 @@ settings = {
     unlimitedStamina = false, -- Flag for unlimited stamina
     SPRINT_BUTTON = mc_sprint_button[2],
     Quality_Of_Life = true,
-        
-    -- Render Settings
+
+    -- HUD Settings
     enableText = true,
-    noText = true,
-    percentsStamina = true,
-    icon = true,
-    textColor = mc_color[1],
-    alpha = mc_sprint_speed[5],
-    yPos= mc_ypos[1],
+    fontalpha = 2.9,
+    coords = Vector(0, 0),
 
     -- Custom Values
     xPos=17
@@ -50,15 +46,15 @@ settings = {
 -------------------
 
 local function save()
-    Isaac.SaveModData(mod, json.encode(settings, "settings"))
+    Isaac.SaveModData(mod, json.encode(settings_SM, "settings"))
 end
 
 local function init()
     if Isaac.HasModData(mod) then
         local data = Isaac.LoadModData(mod)
         data = json.decode(data)
-        for k,v in pairs(data) do settings[k] = v end
-        settings.version = VERSION
+        for k,v in pairs(data) do settings_SM[k] = v end
+        settings_SM.version = VERSION
     end
 
     if not Isaac.HasModData(mod) then
@@ -79,13 +75,13 @@ if ModConfigMenu then
 end
 
 local isSpeeded = false;
-local stamina = settings.MAX_STAMINA;
+local stamina = settings_SM.MAX_STAMINA;
 
 local function isSprinting()
-    if Input.IsButtonPressed(Keyboard.KEY_LEFT_SHIFT, 0) and settings.SPRINT_BUTTON == "LEFT SHIFT" then
+    if Input.IsButtonPressed(Keyboard.KEY_LEFT_SHIFT, 0) and settings_SM.SPRINT_BUTTON == "LEFT SHIFT" then
         return true
     end
-    if Input.IsButtonPressed(Keyboard.KEY_LEFT_CONTROL, 0) and settings.SPRINT_BUTTON == "LEFT CONTROL" then
+    if Input.IsButtonPressed(Keyboard.KEY_LEFT_CONTROL, 0) and settings_SM.SPRINT_BUTTON == "LEFT CONTROL" then
         return true
     end
 end
@@ -93,13 +89,13 @@ end
 function mod:postUpdate()
     local player = Isaac.GetPlayer(0);
 
-    if settings.unlimitedStamina then
-        stamina = settings.MAX_STAMINA - 3;
+    if settings_SM.unlimitedStamina then
+        stamina = settings_SM.MAX_STAMINA - 3;
     end
 
-    if settings.Quality_Of_Life == true then
-        if stamina > settings.MAX_STAMINA then
-            stamina = settings.MAX_STAMINA
+    if settings_SM.Quality_Of_Life == true then
+        if stamina > settings_SM.MAX_STAMINA then
+            stamina = settings_SM.MAX_STAMINA
         end
     end
 
@@ -108,24 +104,24 @@ function mod:postUpdate()
     if (isSprinting() and stamina > 0
     and (Input.IsButtonPressed(Keyboard.KEY_W, 0) or Input.IsButtonPressed(Keyboard.KEY_A, 0) or Input.IsButtonPressed(Keyboard.KEY_S, 0) or Input.IsButtonPressed(Keyboard.KEY_D, 0))) then
 
-        stamina = stamina - settings.STAMINA_ADDER;
+        stamina = stamina - settings_SM.STAMINA_ADDER;
 
         if (isSpeeded == false) then -- Speed up
-            player.MoveSpeed = player.MoveSpeed + settings.SPRINT_SPEED;
+            player.MoveSpeed = player.MoveSpeed + settings_SM.SPRINT_SPEED;
             isSpeeded = true;
         end
     end
 
     if (isSprinting() and stamina == 0 and isSpeeded == true) then
-        player.MoveSpeed = player.MoveSpeed - settings.SPRINT_SPEED;
+        player.MoveSpeed = player.MoveSpeed - settings_SM.SPRINT_SPEED;
         isSpeeded = false;
     end
 
-    if (not(isSprinting()) and stamina < settings.MAX_STAMINA) then
-        stamina = stamina + settings.STAMINA_ADDER;
+    if (not(isSprinting()) and stamina < settings_SM.MAX_STAMINA) then
+        stamina = stamina + settings_SM.STAMINA_ADDER;
 
         if (isSpeeded == true) then -- Speed down
-            player.MoveSpeed = player.MoveSpeed - settings.SPRINT_SPEED;
+            player.MoveSpeed = player.MoveSpeed - settings_SM.SPRINT_SPEED;
             isSpeeded = false;
         end
     end
@@ -135,48 +131,82 @@ mod:AddCallback(ModCallbacks.MC_POST_UPDATE, mod.postUpdate);
 mod.staminaIcon = Sprite();
 mod.staminaIcon:Load("gfx/ui/stamina.anm2", true);
 mod.staminaIcon:Play("Idle", true);
-mod.staminaIcon.Color = Color(1, 1, 1, settings.alpha)
+mod.staminaIcon.Color = Color(1, 1, 1, settings_SM.alpha)
 
 mod.font = Font()
 mod.font:Load("font/luaminioutlined.fnt")
 
-local function onRender(t)
-    local text = "";
-    text = tostring(math.floor(stamina));
+function mod:onRender(shaderName)
+    local valueOutput = math.ceil((math.floor(stamina) * 100) / settings_SM.MAX_STAMINA)
+    if valueOutput == -1 or valueOutput == -2 then valueOutput = 0 end
+    valueOutput = valueOutput.."%"
 
-    if text == "-1" or text == "-2" then text = "0" end
-    
-    if settings.percentsStamina then
-        local percents = math.ceil((math.floor(stamina) * 100) / settings.MAX_STAMINA)
-        if percents == -1 or percents == -2 then percents = 0 end
-        text = tostring(percents).."%"
-    end
-
-    if not settings.noText then text = "Stamina: "..text end
-    --- Color
-    local r = 0;
-    local g = 0;
-    local b = 0;
-    
-    if settings.textColor == "white" then r=1; g=1; b=1; end
-    if settings.textColor == "red" then r=1; g=0; b=0; end
-    if settings.textColor == "green" then r=0; g=1; b=0; end
-    if settings.textColor == "blue" then r=0; g=0; b=1; end
-    if settings.textColor == "magenta" then r=1; g=0; b=1; end
-    if settings.textColor == "cyan" then r=0; g=1; b=1; end
-    if settings.textColor == "yellow" then r=1; g=1; b=0; end
-
-    --- Render
-    if settings.enableText and not Game():IsPaused() then
-        mod.font:DrawString(text, settings.xPos+15, settings.yPos, KColor(r, g, b, settings.alpha), 0, true)
-        if settings.icon then
-            mod.staminaIcon:Render(Vector(settings.xPos-3, settings.yPos-3), Vector(0,0), Vector(0,0));
-        end
+    if settings_SM.enableText and not Game():IsPaused() then
+	    mod:updatePosition()
+	    local textCoords = settings_SM.coords + Game().ScreenShakeOffset
+	    mod.font:DrawString(valueOutput, textCoords.X + 16, textCoords.Y + 1, KColor(1, 1, 1, 0.5), 0, true)
+	    mod.staminaIcon:Render(settings_SM.coords, Vector(0, 0), Vector(0, 0))
     end
 end
-mod:AddCallback(ModCallbacks.MC_POST_RENDER, onRender)
+mod:AddCallback(ModCallbacks.MC_GET_SHADER_PARAMS, mod.onRender)
+mod:AddCallback(ModCallbacks.MC_POST_RENDER, mod.onRender)
 
-function SprintMod:onLevelStart()
-    stamina = settings.MAX_STAMINA
+function mod:updatePosition() -- isaac-planetarium-chance | https://github.com/Sectimus/isaac-planetarium-chance?tab=readme-ov-file
+	local TrueCoopShift = false
+	local BombShift = false
+	local PoopShift = false
+	local RedHeartShift = false
+	local SoulHeartShift = false
+	local DualityShift = false
+
+	local ShiftCount = 0
+
+	settings_SM.coords = Vector(0, 168)
+
+	for i = 0, Game():GetNumPlayers() - 1 do
+		local player = Isaac.GetPlayer(i)
+		local playerType = player:GetPlayerType()
+
+		if player:GetBabySkin() == -1 then
+			if i > 0 and player.Parent == nil and playerType == player:GetMainTwin():GetPlayerType() and not TrueCoopShift then
+				TrueCoopShift = true
+			end
+
+			if playerType ~= PlayerType.PLAYER_BLUEBABY_B and not BombShift then BombShift = true end
+		end
+		if playerType == PlayerType.PLAYER_BLUEBABY_B and not PoopShift then PoopShift = true end
+		if playerType == PlayerType.PLAYER_BETHANY_B and not RedHeartShift then RedHeartShift = true end
+		if playerType == PlayerType.PLAYER_BETHANY and not SoulHeartShift then SoulHeartShift = true end
+		if player:HasCollectible(CollectibleType.COLLECTIBLE_DUALITY) and not DualityShift then DualityShift = true end
+	end
+
+	if BombShift then ShiftCount = ShiftCount + 1 end
+	if PoopShift then ShiftCount = ShiftCount + 1 end
+	if RedHeartShift then ShiftCount = ShiftCount + 1 end
+	if SoulHeartShift then ShiftCount = ShiftCount + 1 end
+	ShiftCount = ShiftCount - 1
+	if ShiftCount > 0 then settings_SM.coords = settings_SM.coords + Vector(0, (11 * ShiftCount) - 2) end
+
+	if Isaac.GetPlayer(0):GetPlayerType() == PlayerType.PLAYER_JACOB then
+		settings_SM.coords = settings_SM.coords + Vector(0, 30)
+	elseif TrueCoopShift then
+		settings_SM.coords = settings_SM.coords + Vector(0, 16)
+		if DualityShift then
+			settings_SM.coords = settings_SM.coords + Vector(0, -2) -- I hate this
+		end
+	end
+	if DualityShift then
+		settings_SM.coords = settings_SM.coords + Vector(0, -12)
+	end
+
+	if Game().Difficulty == Difficulty.DIFFICULTY_HARD or Game():IsGreedMode() or not CanRunUnlockAchievements() then
+		settings_SM.coords = settings_SM.coords + Vector(0, 16)
+	end
+
+	settings_SM.coords = settings_SM.coords + (Options.HUDOffset * Vector(20, 12))
+end
+
+function mod:onLevelStart()
+    stamina = settings_SM.MAX_STAMINA
 end
 mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, mod.onLevelStart)
--- scripts/mcm.lua
+++ scripts/mcm.lua
@@ -1,5 +1,5 @@
 local function addSpace(tab_name)
-    ModConfigMenu.AddSpace(MOD_NAME, tab_name)
+    ModConfigMenu.AddSpace(Sprint.MOD_NAME, tab_name)
 end
 
 local function getTableIndex(tbl, val)
@@ -11,64 +11,77 @@ local function getTableIndex(tbl, val)
     return 0
 end
 
-ModConfigMenu.UpdateCategory(MOD_NAME, {
-    Info = "View settings for " .. MOD_NAME .. ".",
+ModConfigMenu.UpdateCategory(Sprint.MOD_NAME, {
+    Info = "View settings for " .. Sprint.MOD_NAME .. "."
 });
 
-ModConfigMenu.AddTitle(MOD_NAME, "Info", MOD_NAME)
-ModConfigMenu.AddText(MOD_NAME, "Info", function() return "Version " .. VERSION end);
+ModConfigMenu.AddTitle(Sprint.MOD_NAME, "Info", Sprint.MOD_NAME)
+ModConfigMenu.AddText(Sprint.MOD_NAME, "Info", function() return "Sprint.VERSION " .. Sprint.VERSION end);
 addSpace("Info")
-ModConfigMenu.AddTitle(MOD_NAME, "Info", "Developer")
-ModConfigMenu.AddText(MOD_NAME, "Info", function() return AUTHOR end);
-ModConfigMenu.AddText(MOD_NAME, "Info", function() return "Follow me on Instagram: " .. SOCIAL end);
+ModConfigMenu.AddTitle(Sprint.MOD_NAME, "Info", "Developer")
+ModConfigMenu.AddText(Sprint.MOD_NAME, "Info", function() return Sprint.AUTHOR end);
+ModConfigMenu.AddText(Sprint.MOD_NAME, "Info", function() return "Follow me on Instagram: " .. Sprint.SOCIAL end);
 
 addSpace("Settings")
-ModConfigMenu.AddTitle(MOD_NAME, "Settings", "Mod Settings")
+ModConfigMenu.AddTitle(Sprint.MOD_NAME, "Settings", "Mod Settings")
 
-ModConfigMenu.AddSetting(MOD_NAME, "Settings",
+ModConfigMenu.AddSetting(Sprint.MOD_NAME, "Settings",
 {
     Type = ModConfigMenu.OptionType.NUMBER,
     CurrentSetting = function()
-        return getTableIndex(mc_max_stamina, settings.MAX_STAMINA)
+        return getTableIndex(mc_max_stamina, settings_SM.MAX_STAMINA)
     end,
     Minimum = 1,
     Maximum = #mc_max_stamina,
     Display = function()
-        return "Max Stamina: " .. settings.MAX_STAMINA
+        return "Max Stamina: " .. settings_SM.MAX_STAMINA
     end,
     OnChange = function(n)
-        settings.MAX_STAMINA = mc_max_stamina[n]
+        settings_SM.MAX_STAMINA = mc_max_stamina[n]
     end,
 });
-ModConfigMenu.AddSetting(MOD_NAME, "Settings",
+ModConfigMenu.AddSetting(Sprint.MOD_NAME, "Settings",
 {
     Type = ModConfigMenu.OptionType.NUMBER,
     CurrentSetting = function()
-        return getTableIndex(mc_sprint_speed, settings.SPRINT_SPEED)
+        return getTableIndex(mc_sprint_speed, settings_SM.SPRINT_SPEED)
     end,
     Minimum = 1,
     Maximum = #mc_sprint_speed,
     Display = function()
-        return "Sprint Speed: " .. settings.SPRINT_SPEED
+        return "Sprint Speed: " .. settings_SM.SPRINT_SPEED
     end,
     OnChange = function(n)
-        settings.SPRINT_SPEED = mc_sprint_speed[n]
+        settings_SM.SPRINT_SPEED = mc_sprint_speed[n]
     end,
     Info = { 
         "The speed that adds to your base speed when you're sprinting",
     }
 });
-ModConfigMenu.AddSetting(MOD_NAME, "Settings",
+ModConfigMenu.AddSetting(Sprint.MOD_NAME, "Settings",
 {
     Type = ModConfigMenu.OptionType.BOOLEAN,
     CurrentSetting = function()
-        return settings.unlimitedStamina
+        return settings_SM.enableText
     end,
     Display = function()
-        return "Unlimited Stamina: " .. (settings.unlimitedStamina and "on" or "off")
+        return "Show Text in HUD: " .. (settings_SM.enableText and "on" or "off")
     end,
     OnChange = function(b)
-        settings.unlimitedStamina = b
+        settings_SM.enableText = b
+    end
+});
+ModConfigMenu.AddSetting(Sprint.MOD_NAME, "Settings",
+{
+    Type = ModConfigMenu.OptionType.BOOLEAN,
+    CurrentSetting = function()
+        return settings_SM.unlimitedStamina
+    end,
+    Display = function()
+        return "Unlimited Stamina: " .. (settings_SM.unlimitedStamina and "on" or "off")
+    end,
+    OnChange = function(b)
+        settings_SM.unlimitedStamina = b
     end,
     Info = {
         "If you turn on this setting your stamina",
@@ -76,33 +89,33 @@ ModConfigMenu.AddSetting(MOD_NAME, "Settings",
     }
 });
 addSpace("Settings")
-ModConfigMenu.AddTitle(MOD_NAME, "Settings", "Other Settings")
-ModConfigMenu.AddSetting(MOD_NAME, "Settings",
+ModConfigMenu.AddTitle(Sprint.MOD_NAME, "Settings", "Other Settings")
+ModConfigMenu.AddSetting(Sprint.MOD_NAME, "Settings",
 {
     Type = ModConfigMenu.OptionType.NUMBER,
     CurrentSetting = function()
-        return getTableIndex(mc_sprint_button, settings.SPRINT_BUTTON)
+        return getTableIndex(mc_sprint_button, settings_SM.SPRINT_BUTTON)
     end,
     Minimum = 1,
     Maximum = #mc_sprint_button,
     Display = function()
-        return "Sprint Button: " .. settings.SPRINT_BUTTON
+        return "Sprint Button: " .. settings_SM.SPRINT_BUTTON
     end,
     OnChange = function(n)
-        settings.SPRINT_BUTTON = mc_sprint_button[n]
+        settings_SM.SPRINT_BUTTON = mc_sprint_button[n]
     end
 });
-ModConfigMenu.AddSetting(MOD_NAME, "Settings",
+ModConfigMenu.AddSetting(Sprint.MOD_NAME, "Settings",
 {
     Type = ModConfigMenu.OptionType.BOOLEAN,
     CurrentSetting = function()
-        return settings.Quality_Of_Life
+        return settings_SM.Quality_Of_Life
     end,
     Display = function()
-        return "Quality Of Life Settings: " .. (settings.Quality_Of_Life and "on" or "off")
+        return "Quality Of Life Settings: " .. (settings_SM.Quality_Of_Life and "on" or "off")
     end,
     OnChange = function(b)
-        settings.Quality_Of_Life = b
+        settings_SM.Quality_Of_Life = b
     end,
     Info = {
         "It makes the mod more stable.",
@@ -110,20 +123,20 @@ ModConfigMenu.AddSetting(MOD_NAME, "Settings",
     }
 });
 addSpace("Settings")
-ModConfigMenu.AddTitle(MOD_NAME, "Settings", "(!) Experimental Settings (!)")
-ModConfigMenu.AddSetting(MOD_NAME, "Settings",
+ModConfigMenu.AddTitle(Sprint.MOD_NAME, "Settings", "(!) Experimental Settings (!)")
+ModConfigMenu.AddSetting(Sprint.MOD_NAME, "Settings",
 {
     Type = ModConfigMenu.OptionType.NUMBER,
     CurrentSetting = function()
-        return getTableIndex(mc_stamina_adder, settings.STAMINA_ADDER)
+        return getTableIndex(mc_stamina_adder, settings_SM.STAMINA_ADDER)
     end,
     Minimum = 1,
     Maximum = #mc_stamina_adder,
     Display = function()
-        return "Stamina Adder: " .. settings.STAMINA_ADDER
+        return "Stamina Adder: " .. settings_SM.STAMINA_ADDER
     end,
     OnChange = function(n)
-        settings.STAMINA_ADDER = mc_stamina_adder[n]
+        settings_SM.STAMINA_ADDER = mc_stamina_adder[n]
     end,
     Info = {  
         "(!) This is experimental setting (!)",
@@ -131,116 +144,3 @@ ModConfigMenu.AddSetting(MOD_NAME, "Settings",
     }
 });
 addSpace("Settings")
-ModConfigMenu.AddTitle(MOD_NAME, "Render", "Stat Renderer")
-ModConfigMenu.AddSetting(MOD_NAME, "Render",
-{
-    Type = ModConfigMenu.OptionType.BOOLEAN,
-    CurrentSetting = function()
-        return settings.enableText
-    end,
-    Display = function()
-        return "Enabled: " .. (settings.enableText and "on" or "off")
-    end,
-    OnChange = function(b)
-        settings.enableText = b
-    end
-});
-ModConfigMenu.AddSetting(MOD_NAME, "Render",
-{
-    Type = ModConfigMenu.OptionType.BOOLEAN,
-    CurrentSetting = function()
-        return settings.noText
-    end,
-    Display = function()
-        return "No text: " .. (settings.noText and "on" or "off")
-    end,
-    OnChange = function(b)
-        settings.noText = b
-    end,
-    Info = {
-        "It shows only the value of your stamina",
-    }
-});
-ModConfigMenu.AddSetting(MOD_NAME, "Render",
-{
-    Type = ModConfigMenu.OptionType.NUMBER,
-    CurrentSetting = function()
-        return getTableIndex(mc_ypos, settings.yPos)
-    end,
-    Minimum = 1,
-    Maximum = #mc_ypos,
-    Display = function()
-        return "Y Pos: " .. settings.yPos
-    end,
-    OnChange = function(n)
-        settings.yPos = mc_ypos[n]
-    end,
-    Info = { 
-        "It changes the Y position of the text"
-    }
-});
-ModConfigMenu.AddSetting(MOD_NAME, "Render",
-{
-    Type = ModConfigMenu.OptionType.BOOLEAN,
-    CurrentSetting = function()
-        return settings.percentsStamina
-    end,
-    Display = function()
-        return "Show stamina in percents: " .. (settings.percentsStamina and "on" or "off")
-    end,
-    OnChange = function(b)
-        settings.percentsStamina = b
-    end,
-    Info = {
-        "It shows the stamina in percents %",
-    }
-});
-ModConfigMenu.AddSetting(MOD_NAME, "Render",
-{
-    Type = ModConfigMenu.OptionType.BOOLEAN,
-    CurrentSetting = function()
-        return settings.icon
-    end,
-    Display = function()
-        return "Show Icon: " .. (settings.icon and "on" or "off")
-    end,
-    OnChange = function(b)
-        settings.icon = b
-    end
-});
-ModConfigMenu.AddSetting(MOD_NAME, "Render",
-{
-    Type = ModConfigMenu.OptionType.NUMBER,
-    CurrentSetting = function()
-        return getTableIndex(mc_color, settings.textColor)
-    end,
-    Minimum = 1,
-    Maximum = #mc_color,
-    Display = function()
-        return "Text Color: " .. settings.textColor
-    end,
-    OnChange = function(n)
-        settings.textColor = mc_color[n]
-    end,
-    Info = { 
-        "Changes the color of the text",
-    }
-});
-ModConfigMenu.AddSetting(MOD_NAME, "Render",
-{
-    Type = ModConfigMenu.OptionType.NUMBER,
-    CurrentSetting = function()
-        return getTableIndex(mc_sprint_speed, settings.alpha)
-    end,
-    Minimum = 1,
-    Maximum = #mc_sprint_speed,
-    Display = function()
-        return "Alpha: " .. settings.alpha
-    end,
-    OnChange = function(n)
-        settings.alpha = mc_sprint_speed[n]
-    end,
-    Info = { 
-        "Transparency of a color (*0.5)"
-    }
-});
